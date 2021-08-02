a88e99243fb8b3c9c56f47258693524e134f36e1
VivekG11 <vivekvk2903@gmail.com>
2021-08-03 01:11:06 +0530

    [Adding data into table]

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -2,3 +2,15 @@ create database AddressBook;
 
 use AddressBook;
 
+create table Addressbooktable
+(
+FirstName varchar(150),
+LastName varchar(100),
+address varchar(300),
+city varchar (50),
+state varchar (50),
+Zip bigint,
+PhoneNumber bigint,
+email varchar (300)
+)
+select *from Addressbooktable;
